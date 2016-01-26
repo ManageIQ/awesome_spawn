ManageIQ/awesome_spawn

commit 8ce400715ce9a6c83107a25bab0b8b8c569b4f44

remove unnecessary code per mutating specs

lib/awesome_spawn/command_line_builder.rb
@@ -54,7 +54,7 @@ module AwesomeSpawn
 
     def sanitize(params)
       return [] if params.nil? || params.empty?
-      sanitize_associative_array(params.to_a)
+      sanitize_associative_array(params)
     end
 
     def sanitize_associative_array(assoc_array)
@@ -66,8 +66,8 @@ module AwesomeSpawn
     def sanitize_item(item)
       case item
       when Array then sanitize_key_values(item[0], item[1..-1])
-      when Hash  then sanitize_associative_array(item.to_a)
-      else            sanitize_key_values(item, [])
+      when Hash  then sanitize_associative_array(item)
+      else            sanitize_key_values(item, nil)
       end
     end
 
@@ -78,12 +78,11 @@ module AwesomeSpawn
     KEY_REGEX = /^((?:--?)?)(.+?)(=?)$/
 
     def sanitize_key(key)
-      return key if key.nil?
+      return key if key.nil? || key.empty?
       key = convert_symbol_key(key) if key.kind_of?(Symbol)
 
       case key
       when String
-        return key if key.empty?
         prefix, key, suffix = KEY_REGEX.match(key)[1..3]
         "#{prefix}#{sanitize_value(key)}#{suffix}"
       else
@@ -98,7 +97,7 @@ module AwesomeSpawn
     def sanitize_value(value)
       case value
       when Enumerable
-        value.to_a.collect { |i| sanitize_value(i) }.compact
+        value.collect { |i| sanitize_value(i) }.compact
       when NilClass
         value
       else

lib/awesome_spawn.rb
@@ -64,7 +64,6 @@ module AwesomeSpawn
       options[:stdin_data] = in_data
     end
 
-    output, error, status = "", "", nil
     command_line = build_command_line(command, params)
 
     output, error, status = launch(command_line, options)
@@ -104,8 +103,8 @@ module AwesomeSpawn
 
   private
 
-  def launch(command, spawn_options = {})
+  def launch(command, spawn_options)
     output, error, status = Open3.capture3(command, spawn_options)
-    return output || "", error || "", status && status.exitstatus
+    return output, error, status && status.exitstatus
   end
 end

commit bb0566d47652f182c5d49abb23634bcdade649ea

Fix support for Pathnames in options broken by 8ce400715ce

lib/awesome_spawn/command_line_builder.rb
@@ -83,7 +83,7 @@ module AwesomeSpawn
     KEY_REGEX = /^((?:--?)?)(.+?)(=?)$/
 
     def sanitize_key(key)
-      return key if key.nil? || key.empty?
+      return key if key.nil? || key.to_s.empty?
       key = convert_symbol_key(key) if key.kind_of?(Symbol)
 
       case key

spec/command_line_builder_spec.rb
@@ -34,6 +34,13 @@ describe AwesomeSpawn::CommandLineBuilder do
       expect(actual).to eq "/usr/bin/ruby -v"
     end
 
+    it "with Pathname in params" do
+      options = ["-d", Pathname.new("script.rb")]
+
+      actual = subject.build(Pathname.new("/usr/bin/ruby"), options)
+      expect(actual).to eq "/usr/bin/ruby -d script.rb"
+    end
+
     context "with Hash" do
       it "that is empty" do
         assert_params({}, "")