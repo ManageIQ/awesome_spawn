bf871944912f094dfc158bc22a91dbca7d4dce10
Nick Carboni <ncarboni@redhat.com>
2019-06-24 14:00:15 -0400

    Add failing spec to display issues with the spec helper methods

---
+++ spec/spec_helper_spec.rb
@@ -0,0 +1,13 @@
+describe AwesomeSpawn::SpecHelper do
+  describe ".stub_good_run" do
+    it "works" do
+      described_class.stub_good_run
+    end
+  end
+
+  describe ".stub_bad_run" do
+    it "works" do
+      described_class.stub_bad_run
+    end
+  end
+end
