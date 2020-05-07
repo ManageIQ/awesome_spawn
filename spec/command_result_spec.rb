2d9b0f2196ac58a110c175265cdc34ff23a11c62
d-m-u <drewuhlmann@gmail.com>
2020-05-07 14:06:06 -0400

    add pid to command result
    i'm fixing a brakeman warning in system_console about `pid = spawn(*command)`
    and i'd like to have command result be able to return the pid

--- spec/command_result_spec.rb
+++ spec/command_result_spec.rb
@@ -2,13 +2,14 @@ require 'spec_helper'
 
 describe AwesomeSpawn::CommandResult do
   context "succeeding object" do
-    subject { described_class.new("aaa", "bbb", "ccc", 0) }
+    subject { described_class.new("aaa", "bbb", "ccc", 10_000, 0) }
 
     it "should set attributes" do
       expect(subject.command_line).to eq("aaa")
       expect(subject.output).to eq("bbb")
       expect(subject.error).to eq("ccc")
       expect(subject.exit_status).to eq(0)
+      expect(subject.pid).to eq(10_000)
       expect(subject.inspect).to match(/^#<AwesomeSpawn::CommandResult:[0-9a-fx]+ @exit_status=0>$/)
     end
 
@@ -23,14 +24,14 @@ describe AwesomeSpawn::CommandResult do
   end
 
   context "failing object" do
-    subject { described_class.new("aaa", "bbb", "ccc", 1) }
+    subject { described_class.new("aaa", "bbb", "ccc", 10_001, 1) }
 
     it { expect(subject).not_to be_a_success }
     it { expect(subject).to be_a_failure }
   end
 
   context "another failing object" do
-    subject { described_class.new("aaa", "bbb", "ccc", 100) }
+    subject { described_class.new("aaa", "bbb", "ccc", 10_002, 100) }
 
     it { expect(subject).not_to be_a_success }
     it { expect(subject).to be_a_failure }
